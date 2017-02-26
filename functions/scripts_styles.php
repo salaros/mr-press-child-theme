4e608425de5f4259383254c3a9fff40ef71a48ca
salaros <salaros@yandex.ru>
2017-02-27 01:33:05 +0300

    implemented easy to use WP toolkit switch
    added Semantic UI dependency via a Bower asset

--- functions/scripts_styles.php
+++ functions/scripts_styles.php
@@ -3,15 +3,30 @@
 $bower_url = $site->theme->bower_url;
 
 //this is where you can add your javascript entries
-$site->add_script( 'respond', sprintf( '%s/respond/dest/respond.min.js', $bower_url ), [], '1.4.2', false, ['conditional' => 'lt IE 9'] );
-$site->add_script( 'html5shiv', sprintf( '%s/html5shiv/dist/html5shiv.min.js', $bower_url), [], '3.7.3', false, ['conditional' => 'lt IE 9'] );
-$site->add_script( 'bootstrap', sprintf( '%s/bootstrap/dist/js/bootstrap.min.js', $bower_url), ['jquery'], '3.3.6', true );
-$site->add_script( 'site', sprintf( '%s/static/js/site.js', $site->theme->link) , ['bootstrap'], '0.2.1', true );
+$site->add_script( 'site', sprintf( '%s/static/js/site.js', $site->theme->link ) , [ 'bootstrap' ], '0.2.1', true );
 
 //this is where you can add your CSS entries
-$site->add_style( 'normalize', sprintf( '%s/normalize.css/normalize.css', $bower_url ) );
-$site->add_style( 'bootstrap', sprintf( '%s/bootstrap/dist/css/bootstrap.min.css', $bower_url ) );
-$site->add_style( 'mr-press', sprintf( '%s/style.css', $site->theme->parent->link ) );
+$site->add_style( 'normalize', sprintf( '%s/normalize.css/normalize.css', $bower_url, [], '3.0.3' ) );
 
 //this is where you can add your CSS entries for wp-admin UI
 $site->add_admin_style( 'custom-admin', sprintf( '%s/css/admin.css', $site->theme->static_url ) );
+
+switch ( $site->ui_toolkit ) {
+	case 'bootstrap':
+		$site->add_script( 'respond', sprintf( '%s/respond/dest/respond.min.js', $bower_url ), [], '1.4.2', false, [ 'conditional' => 'lt IE 9' ] );
+		$site->add_script( 'html5shiv', sprintf( '%s/html5shiv/dist/html5shiv.min.js', $bower_url ), [], '3.7.3', false, [ 'conditional' => 'lt IE 9' ] );
+		$site->add_script( 'bootstrap', sprintf( '%s/bootstrap/dist/js/bootstrap.min.js', $bower_url ), [ 'jquery' ], '3.3.7', true );
+		$site->add_style( 'bootstrap', sprintf( '%s/bootstrap/dist/css/bootstrap.min.css', $bower_url, [ 'normalize' ] ) );
+		$site->add_style( 'ui-toolkit', sprintf( '%s/style-%s.css', $site->theme->link, $site->ui_toolkit ), [ 'bootstrap' ] );
+		break;
+
+	case 'semantic-ui':
+		$site->add_script( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.js', $bower_url ), [ 'jquery' ], '2.2.9', true );
+		$site->add_style( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.css', $bower_url ), [ 'normalize' ] );
+		$site->add_style( 'ui-toolkit', sprintf( '%s/style-%s.css', $site->theme->link, $site->ui_toolkit ), [ 'semantic-ui' ] );
+		break;
+
+	default:
+		trigger_error( sprintf( 'Unknown UI kit, please edit "%s" file', realpath( __FILE__ ) ) );
+		break;
+}
