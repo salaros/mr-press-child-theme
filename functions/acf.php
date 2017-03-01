fd250747511e8fcd5226a55671e33d44572e8e64
salaros <salaros@yandex.ru>
2017-03-01 21:16:29 +0300

    adding a possibility to inject ACF entries directly from theme's code
    ACF = Advanced Custom Fields plugin

---
+++ functions/acf.php
@@ -0,0 +1,28 @@
+<?php
+
+if ( ! function_exists( 'register_field_group' ) ) {
+	return;
+}
+
+register_field_group(
+	[
+		'id' => 'acf_test-group',
+		'title' => 'Test group',
+		'fields' => [
+			[
+				'key' => 'field_1000000000000',
+				'label' => 'Test bool',
+				'name' => 'test_bool',
+				'type' => 'true_false',
+				'message' => '',
+				'default_value' => 0,
+			],
+		],
+		'options' => [ 
+		'position' => 'normal',
+		'layout' => 'no_box',
+		'hide_on_screen' => [],
+		],
+		'menu_order' => 0,
+	]
+);
