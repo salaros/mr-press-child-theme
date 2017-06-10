37795a83ffbb6416c68161a671a0ad872e373890
salaros <salaros@yandex.ru>
2017-06-11 02:45:03 +0300

    now script and style management is done via newly created Asset_Manager class

--- functions/scripts_styles.php
+++ functions/scripts_styles.php
@@ -3,29 +3,29 @@
 $bower_url = $site->theme->bower_url;
 
 //this is where you can add your javascript entries
-$site->add_script( 'site', sprintf( '%s/static/js/site.js', $site->theme->link ) , [ 'bootstrap' ], '0.2.1', true );
+$site->assets->add_script( 'site', sprintf( '%s/static/js/site.js', $site->theme->link ) , [ 'bootstrap' ], '0.2.1', true );
 
 //this is where you can add your CSS entries
-$site->add_style( 'normalize', sprintf( '%s/normalize.css/normalize.css', $bower_url, [], '3.0.3' ) );
+$site->assets->add_style( 'normalize', sprintf( '%s/normalize.css/normalize.css', $bower_url, [], '3.0.3' ) );
 
 //this is where you can add your CSS entries for wp-admin UI
-$site->add_admin_style( 'custom-admin', sprintf( '%s/css/admin.css', $site->theme->static_url ) );
+$site->assets->add_admin_style( 'custom-admin', sprintf( '%s/css/admin.css', $site->theme->static_url ) );
 
 switch ( $site->ui_toolkit ) {
 	case 'bootstrap':
-		$site->add_script( 'respond', sprintf( '%s/respond/dest/respond.min.js', $bower_url ), [], '1.4.2', false, [ 'conditional' => 'lt IE 9' ] );
-		$site->add_script( 'html5shiv', sprintf( '%s/html5shiv/dist/html5shiv.min.js', $bower_url ), [], '3.7.3', false, [ 'conditional' => 'lt IE 9' ] );
-		$site->add_script( 'bootstrap', sprintf( '%s/bootstrap/dist/js/bootstrap.min.js', $bower_url ), [ 'jquery' ], '3.3.7', true );
-		$site->add_style( 'bootstrap', sprintf( '%s/bootstrap/dist/css/bootstrap.min.css', $bower_url, [ 'normalize' ] ) );
-		$site->add_script( 'mr-press', sprintf( '%s/%s/mr-press.js', $site->theme->link, $site->ui_toolkit ), [ 'bootstrap' ], null, true );
-		$site->add_style( 'mr-press', sprintf( '%s/%s/mr-press.css', $site->theme->link, $site->ui_toolkit ), [ 'bootstrap' ] );
+		$site->assets->add_script( 'respond', sprintf( '%s/respond/dest/respond.min.js', $bower_url ), [], '1.4.2', false, [ 'conditional' => 'lt IE 9' ] );
+		$site->assets->add_script( 'html5shiv', sprintf( '%s/html5shiv/dist/html5shiv.min.js', $bower_url ), [], '3.7.3', false, [ 'conditional' => 'lt IE 9' ] );
+		$site->assets->add_script( 'bootstrap', sprintf( '%s/bootstrap/dist/js/bootstrap.min.js', $bower_url ), [ 'jquery' ], '3.3.7', true );
+		$site->assets->add_style( 'bootstrap', sprintf( '%s/bootstrap/dist/css/bootstrap.min.css', $bower_url, [ 'normalize' ] ) );
+		$site->assets->add_script( 'mr-press', sprintf( '%s/%s/mr-press.js', $site->theme->link, $site->ui_toolkit ), [ 'bootstrap' ], null, true );
+		$site->assets->add_style( 'mr-press', sprintf( '%s/%s/mr-press.css', $site->theme->link, $site->ui_toolkit ), [ 'bootstrap' ] );
 		break;
 
 	case 'semantic-ui':
-		$site->add_script( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.js', $bower_url ), [ 'jquery' ], '2.2.9', true );
-		$site->add_style( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.css', $bower_url ), [ 'normalize' ] );
-		$site->add_script( 'mr-press', sprintf( '%s/%s/mr-press.js', $site->theme->link, $site->ui_toolkit ), [ 'semantic-ui' ], null, true );
-		$site->add_style( 'mr-press', sprintf( '%s/%s/mr-press.css', $site->theme->link, $site->ui_toolkit ), [ 'semantic-ui' ] );
+		$site->assets->add_script( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.js', $bower_url ), [ 'jquery' ], '2.2.9', true );
+		$site->assets->add_style( 'semantic-ui', sprintf( '%s/semantic/dist/semantic.min.css', $bower_url ), [ 'normalize' ] );
+		$site->assets->add_script( 'mr-press', sprintf( '%s/%s/mr-press.js', $site->theme->link, $site->ui_toolkit ), [ 'semantic-ui' ], null, true );
+		$site->assets->add_style( 'mr-press', sprintf( '%s/%s/mr-press.css', $site->theme->link, $site->ui_toolkit ), [ 'semantic-ui' ] );
 		break;
 
 	default:
