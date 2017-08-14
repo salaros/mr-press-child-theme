d9c2714df78ba1ec0aaaa809070cde2bd9e3824e
salaros <salaros@yandex.ru>
2017-08-14 12:53:58 +0300

    improving ajax sample script

--- functions/ajax.php
+++ functions/ajax.php
@@ -5,36 +5,41 @@ add_action( 'wp_ajax_nopriv_some_ajax_function', 'mr_press_some_ajax_function' )
 
 function mr_press_some_ajax_function() {
 
-	check_ajax_referer( 'ajax-verification-string', 'security' );
+	check_ajax_referer( 'security_0123456789', 'survey_nonce' );
 
 	if ( ! isset( $_POST['myvar'] ) || empty( $_POST['myvar'] ) ) {
-		$myvar = sanitize_text_field( $_POST['my_string'] );
-		$error = [
-			'error' => true,
-			'message' => 'myvar is empty',
-		];
-		exit( wp_json_encode( $error ) );
+		echo( wp_json_encode(
+			[
+				'error' => true,
+				'message' => 'myvar is empty',
+			]
+		) );
+		return;
 	}
 
-	$error = [
-		'error' => false,
-		'result' => 'OK',
-	];
-	exit( wp_json_encode( $error ) );
+	$my_string = sanitize_text_field( $_POST['my_string'] );
+	echo( wp_json_encode(
+		[
+			'error' => false,
+			'result' => 'OK',
+			'myvar' => $my_string,
+		]
+	) );
+	return;
 }
 
 /****************   HOW TO USE IT   ******************
 
 <?php
 //Set Your Nonce
-$ajax_nonce = wp_create_nonce( "ajax-verification-string" );
+$ajax_nonce = wp_create_nonce( "security_0123456789" );
 ?>
 
 <script type="text/javascript">
 jQuery(document).ready(function($){
 	var data = {
 		action: 'my_action',
-		security: '<?php echo $ajax_nonce; ?>',
+		survey_nonce: '<?php echo $ajax_nonce; ?>',
 		my_string: 'Hello World!'
 	};
 	$.post(ajaxurl, data, function(response) {
