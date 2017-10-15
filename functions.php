c344af65c71d5b1ee9db9f75411b6a49386c2e27
salaros <salaros@yandex.ru>
2017-10-16 01:32:58 +0300

    moving frontend-related stuff into "not ajax" block

--- functions.php
+++ functions.php
@@ -11,17 +11,18 @@ $site = new WordPressSite;
 $site->ui_toolkit = 'semantic-ui';
 
 require_once 'functions/constants.php';
-require_once 'functions/menus.php';
 require_once 'functions/post_types.php';
 require_once 'functions/scripts_styles.php';
 require_once 'functions/taxonomies.php';
-require_once 'functions/theme_support.php';
 require_once 'functions/tweaks.php';
-require_once 'functions/context.php';
-require_once 'functions/twig.php';
 
 if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 	include_once 'functions/ajax.php';
+} else {
+	require_once 'functions/menus.php';
+	require_once 'functions/theme_support.php';
+	require_once 'functions/context.php';
+	require_once 'functions/twig.php';
 }
 
 $site->initialize();
