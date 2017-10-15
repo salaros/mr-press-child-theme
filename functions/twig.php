eff4a42937e190a8aa58367dd6f93d7eb7ee65c1
salaros <salaros@yandex.ru>
2017-10-15 21:46:06 +0300

    enable Twig file content caching on production instances

--- functions/twig.php
+++ functions/twig.php
@@ -2,5 +2,5 @@
 
 // Enable Timber (Twig) cache on non-production environments
 if ( class_exists( 'Timber' ) ) {
-	Timber::$cache = false; // (WP_ENV === 'production');
+	Timber::$cache = ( defined( 'WP_ENV' ) && 'production' === WP_ENV );
 }
